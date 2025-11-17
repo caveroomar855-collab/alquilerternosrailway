8675596162ea2f469f83d258550a3c9e83538e85
MARCEKMC <iakevinmendoza@gmail.com>
2025-11-16 23:43:36 -1100

    a

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -2,19 +2,29 @@ import fastifyPlugin from 'fastify-plugin';
 import fastifyJwt from '@fastify/jwt';
 import { config } from '../config.js';
 
+const localDeviceUser = {
+  sub: 'local-device',
+  username: 'Penguin Local',
+  role: 'ADMIN',
+};
+
 async function authPlugin(fastify) {
   await fastify.register(fastifyJwt, {
     secret: config.jwtSecret,
     sign: { expiresIn: config.jwtExpiresIn },
   });
 
-  fastify.decorate('authenticate', async (request, reply) => {
-    try {
-      await request.jwtVerify();
-    } catch (err) {
-      reply.code(401).send({ message: 'No autorizado' });
-      throw err;
+  fastify.decorate('authenticate', async (request) => {
+    const authHeader = request.headers.authorization;
+    if (authHeader) {
+      try {
+        await request.jwtVerify();
+        return;
+      } catch (err) {
+        request.log.warn({ err }, 'Invalid token, defaulting to local-device user');
+      }
     }
+    request.user = { ...localDeviceUser };
   });
 }
 
